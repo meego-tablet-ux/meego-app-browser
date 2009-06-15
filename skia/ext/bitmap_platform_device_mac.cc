meego-tablet-ux/meego-app-browser

commit 999d0b7abd6a479358c1ad476e5f563647797d67

Refactor the PlatformContext layer to have only one class.
Previously we had three classes of PlatformCanvas*, one for each platform. Then
we had a typedef of PlatformContext to PlatformCanvas[Mac|Win|Linux] for the
specific platform.

This means that it was almost impossible to forward-declare PlatformCanvas and
there were a bunch of unnecessary includes of platform_canvas.h in header
files.

This change makes there be only one platform_canvas.h header with ifdefs, which
removes a decent amount of duplicated code. There is a platform-independent
file, and one platform-dependent file of platform_canvas for each platform.

I also renamed PlatformDevice[Mac|Win|Linux] to PlatformDevice, althouth in
this case I kept the separate headers since there was much less overlap.

I also broke out CanvasPaint into separate headers so this template doesn't
need to be included all over the project (only a couple of files actually need
it).

Review URL: http://codereview.chromium.org/125109

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18363 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: c40e89248c9430a0f95dce56a5af015759a70f45

skia/ext/bitmap_platform_device_mac.cc
@@ -46,9 +46,9 @@ bool Constrain(int available_size, int* position, int *size) {
 
 }  // namespace
 
-class BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData : public SkRefCnt {
+class BitmapPlatformDevice::BitmapPlatformDeviceData : public SkRefCnt {
  public:
-  explicit BitmapPlatformDeviceMacData(CGContextRef bitmap);
+  explicit BitmapPlatformDeviceData(CGContextRef bitmap);
 
   // Create/destroy CoreGraphics context for our bitmap data.
   CGContextRef GetBitmapContext() {
@@ -88,19 +88,18 @@ class BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData : public SkRefCnt {
   SkRegion clip_region_;
 
  private:
-  friend class base::RefCounted<BitmapPlatformDeviceMacData>;
-  ~BitmapPlatformDeviceMacData() {
+  friend class base::RefCounted<BitmapPlatformDeviceData>;
+  ~BitmapPlatformDeviceData() {
     if (bitmap_context_)
       CGContextRelease(bitmap_context_);
   }
 
   // Disallow copy & assign.
-  BitmapPlatformDeviceMacData(const BitmapPlatformDeviceMacData&);
-  BitmapPlatformDeviceMacData& operator=(const BitmapPlatformDeviceMacData&);
+  BitmapPlatformDeviceData(const BitmapPlatformDeviceData&);
+  BitmapPlatformDeviceData& operator=(const BitmapPlatformDeviceData&);
 };
 
-BitmapPlatformDeviceMac::\
-    BitmapPlatformDeviceMacData::BitmapPlatformDeviceMacData(
+BitmapPlatformDevice::BitmapPlatformDeviceData::BitmapPlatformDeviceData(
     CGContextRef bitmap)
     : bitmap_context_(bitmap),
       config_dirty_(true) {  // Want to load the config next time.
@@ -119,7 +118,7 @@ BitmapPlatformDeviceMac::\
   CGContextSaveGState(bitmap_context_);
 }
 
-void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::SetMatrixClip(
+void BitmapPlatformDevice::BitmapPlatformDeviceData::SetMatrixClip(
     const SkMatrix& transform,
     const SkRegion& region) {
   transform_ = transform;
@@ -127,7 +126,7 @@ void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::SetMatrixClip(
   config_dirty_ = true;
 }
 
-void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::LoadConfig() {
+void BitmapPlatformDevice::BitmapPlatformDeviceData::LoadConfig() {
   if (!config_dirty_ || !bitmap_context_)
     return;  // Nothing to do.
   config_dirty_ = false;
@@ -154,10 +153,10 @@ void BitmapPlatformDeviceMac::BitmapPlatformDeviceMacData::LoadConfig() {
 // that we can create the pixel data before calling the constructor. This is
 // required so that we can call the base class' constructor with the pixel
 // data.
-BitmapPlatformDeviceMac* BitmapPlatformDeviceMac::Create(CGContextRef context,
-                                                         int width,
-                                                         int height,
-                                                         bool is_opaque) {
+BitmapPlatformDevice* BitmapPlatformDevice::Create(CGContextRef context,
+                                                   int width,
+                                                   int height,
+                                                   bool is_opaque) {
   SkBitmap bitmap;
   bitmap.setConfig(SkBitmap::kARGB_8888_Config, width, height);
   if (bitmap.allocPixels() != true)
@@ -197,51 +196,51 @@ BitmapPlatformDeviceMac* BitmapPlatformDeviceMac::Create(CGContextRef context,
   }
 
   // The device object will take ownership of the graphics context.
-  return new BitmapPlatformDeviceMac(
-      new BitmapPlatformDeviceMacData(context), bitmap);
+  return new BitmapPlatformDevice(
+      new BitmapPlatformDeviceData(context), bitmap);
 }
 
 // The device will own the bitmap, which corresponds to also owning the pixel
 // data. Therefore, we do not transfer ownership to the SkDevice's bitmap.
-BitmapPlatformDeviceMac::BitmapPlatformDeviceMac(
-    BitmapPlatformDeviceMacData* data, const SkBitmap& bitmap)
-    : PlatformDeviceMac(bitmap),
+BitmapPlatformDevice::BitmapPlatformDevice(
+    BitmapPlatformDeviceData* data, const SkBitmap& bitmap)
+    : PlatformDevice(bitmap),
       data_(data) {
 }
 
 // The copy constructor just adds another reference to the underlying data.
 // We use a const cast since the default Skia definitions don't define the
 // proper constedness that we expect (accessBitmap should really be const).
-BitmapPlatformDeviceMac::BitmapPlatformDeviceMac(
-    const BitmapPlatformDeviceMac& other)
-    : PlatformDeviceMac(
-          const_cast<BitmapPlatformDeviceMac&>(other).accessBitmap(true)),
+BitmapPlatformDevice::BitmapPlatformDevice(
+    const BitmapPlatformDevice& other)
+    : PlatformDevice(
+          const_cast<BitmapPlatformDevice&>(other).accessBitmap(true)),
       data_(other.data_) {
   data_->ref();
 }
 
-BitmapPlatformDeviceMac::~BitmapPlatformDeviceMac() {
+BitmapPlatformDevice::~BitmapPlatformDevice() {
   data_->unref();
 }
 
-BitmapPlatformDeviceMac& BitmapPlatformDeviceMac::operator=(
-    const BitmapPlatformDeviceMac& other) {
+BitmapPlatformDevice& BitmapPlatformDevice::operator=(
+    const BitmapPlatformDevice& other) {
   data_ = other.data_;
   data_->ref();
   return *this;
 }
 
-CGContextRef BitmapPlatformDeviceMac::GetBitmapContext() {
+CGContextRef BitmapPlatformDevice::GetBitmapContext() {
   return data_->GetBitmapContext();
 }
 
-void BitmapPlatformDeviceMac::setMatrixClip(const SkMatrix& transform,
-                                            const SkRegion& region) {
+void BitmapPlatformDevice::setMatrixClip(const SkMatrix& transform,
+                                         const SkRegion& region) {
   data_->SetMatrixClip(transform, region);
 }
 
-void BitmapPlatformDeviceMac::DrawToContext(CGContextRef context, int x, int y,
-                                            const CGRect* src_rect) {
+void BitmapPlatformDevice::DrawToContext(CGContextRef context, int x, int y,
+                                         const CGRect* src_rect) {
   bool created_dc = false;
   if (!data_->bitmap_context_) {
     created_dc = true;
@@ -273,20 +272,20 @@ void BitmapPlatformDeviceMac::DrawToContext(CGContextRef context, int x, int y,
 }
 
 // Returns the color value at the specified location.
-SkColor BitmapPlatformDeviceMac::getColorAt(int x, int y) {
+SkColor BitmapPlatformDevice::getColorAt(int x, int y) {
   const SkBitmap& bitmap = accessBitmap(true);
   SkAutoLockPixels lock(bitmap);
   uint32_t* data = bitmap.getAddr32(0, 0);
   return static_cast<SkColor>(data[x + y * width()]);
 }
 
-void BitmapPlatformDeviceMac::onAccessBitmap(SkBitmap*) {
+void BitmapPlatformDevice::onAccessBitmap(SkBitmap*) {
   // Not needed in CoreGraphics
 }
 
-void BitmapPlatformDeviceMac::processPixels(int x, int y,
-                                            int width, int height,
-                                            adjustAlpha adjustor) {
+void BitmapPlatformDevice::processPixels(int x, int y,
+                                         int width, int height,
+                                         adjustAlpha adjustor) {
   const SkBitmap& bitmap = accessBitmap(true);
   SkMatrix& matrix = data_->transform_;
   int bitmap_start_x = SkScalarRound(matrix.getTranslateX()) + x;

app/gfx/canvas.h
@@ -208,10 +208,6 @@ class Canvas : public skia::PlatformCanvas {
   DISALLOW_COPY_AND_ASSIGN(Canvas);
 };
 
-#if defined(OS_WIN) || defined(OS_LINUX)
-typedef skia::CanvasPaintT<Canvas> CanvasPaint;
-#endif
-
 }  // namespace gfx;
 
 #endif  // #ifndef APP_GFX_CANVAS_H_

app/gfx/canvas_linux.cc
@@ -4,7 +4,9 @@
 
 #include "app/gfx/canvas.h"
 
+#include <cairo/cairo.h>
 #include <pango/pango.h>
+#include <pango/pangocairo.h>
 
 #include "app/gfx/font.h"
 #include "base/gfx/rect.h"
@@ -44,10 +46,10 @@ PangoFontDescription* PangoFontFromGfxFont(const gfx::Font& gfx_font) {
 namespace gfx {
 
 Canvas::Canvas(int width, int height, bool is_opaque)
-    : skia::PlatformCanvasLinux(width, height, is_opaque) {
+    : skia::PlatformCanvas(width, height, is_opaque) {
 }
 
-Canvas::Canvas() : skia::PlatformCanvasLinux() {
+Canvas::Canvas() : skia::PlatformCanvas() {
 }
 
 Canvas::~Canvas() {
@@ -58,7 +60,7 @@ void Canvas::SizeStringInt(const std::wstring& text,
                            const gfx::Font& font,
                            int* width, int* height, int flags) {
   cairo_surface_t* surface =
-    cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 0, 0);
+      cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 0, 0);
   cairo_t* cr = cairo_create(surface);
   PangoLayout* layout = pango_cairo_create_layout(cr);
 

app/gfx/canvas_paint.h
@@ -1,0 +1,22 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef APP_GFX_CANVAS_PAINT_H_
+#define APP_GFX_CANVAS_PAINT_H_
+
+#include "app/gfx/canvas.h"
+#include "skia/ext/canvas_paint.h"
+
+// Define a skia::CanvasPaint type that wraps our gfx::Canvas like the
+// skia::PlatformCanvasPaint wraps PlatformCanvas.
+
+namespace gfx {
+
+#if defined(OS_WIN) || defined(OS_LINUX)
+typedef skia::CanvasPaintT<Canvas> CanvasPaint;
+#endif
+
+}  // namespace gfx
+
+#endif  // APP_GFX_CANVAS_PAINT_H_

app/gfx/canvas_win.cc
@@ -120,10 +120,10 @@ int ComputeFormatFlags(int flags, const std::wstring& text) {
 namespace gfx {
 
 Canvas::Canvas(int width, int height, bool is_opaque)
-    : skia::PlatformCanvasWin(width, height, is_opaque) {
+    : skia::PlatformCanvas(width, height, is_opaque) {
 }
 
-Canvas::Canvas() : skia::PlatformCanvasWin() {
+Canvas::Canvas() : skia::PlatformCanvas() {
 }
 
 Canvas::~Canvas() {

base/gfx/native_theme.cc
@@ -330,7 +330,7 @@ HRESULT NativeTheme::PaintScrollbarTrack(
     int classic_state,
     RECT* target_rect,
     RECT* align_rect,
-    skia::PlatformCanvasWin* canvas) const {
+    skia::PlatformCanvas* canvas) const {
   HANDLE handle = GetThemeHandle(SCROLLBAR);
   if (handle && draw_theme_)
     return draw_theme_(handle, hdc, part_id, state_id, target_rect, NULL);
@@ -401,7 +401,7 @@ HRESULT NativeTheme::PaintTrackbar(HDC hdc,
                                    int state_id,
                                    int classic_state,
                                    RECT* rect,
-                                   skia::PlatformCanvasWin* canvas) const {
+                                   skia::PlatformCanvas* canvas) const {
   // Make the channel be 4 px thick in the center of the supplied rect.  (4 px
   // matches what XP does in various menus; GetThemePartSize() doesn't seem to
   // return good values here.)

base/gfx/native_theme.h
@@ -18,7 +18,7 @@
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace skia {
-class PlatformCanvasWin;
+class PlatformCanvas;
 }  // namespace skia
 
 namespace gfx {
@@ -174,7 +174,7 @@ class NativeTheme {
                               int classic_state,
                               RECT* target_rect,
                               RECT* align_rect,
-                              skia::PlatformCanvasWin* canvas) const;
+                              skia::PlatformCanvas* canvas) const;
 
   // Paints a scrollbar thumb or gripper.
   HRESULT PaintScrollbarThumb(HDC hdc,
@@ -205,7 +205,7 @@ class NativeTheme {
                         int state_id,
                         int classic_state,
                         RECT* rect,
-                        skia::PlatformCanvasWin* canvas) const;
+                        skia::PlatformCanvas* canvas) const;
 
   bool IsThemingActive() const;
 

chrome/browser/gtk/download_item_gtk.cc
@@ -5,7 +5,7 @@
 #include "chrome/browser/gtk/download_item_gtk.h"
 
 #include "app/l10n_util.h"
-#include "app/gfx/canvas.h"
+#include "app/gfx/canvas_paint.h"
 #include "app/gfx/font.h"
 #include "app/gfx/text_elider.h"
 #include "app/resource_bundle.h"

commit 1ad5b775c6787b4305ae39005e78cee9fd6e8c4d

Stop leaking CGBitmapContextRefs.  Regression from r18363.
BUG=14105
TEST=sh tools/valgrind/chrome_tests.sh --build_dir xcodebuild/Debug --test unit --gtest_filter=RenderWidgetHostTest.Resize
Review URL: http://codereview.chromium.org/126159

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18452 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 23308d4bd33741e88d4a68da625dcbc0828c146e

skia/ext/bitmap_platform_device_mac.cc
@@ -209,10 +209,17 @@ BitmapPlatformDevice* BitmapPlatformDevice::Create(CGContextRef context,
 
   if (!context)
     context = CGContextForData(data, width, height);
+  else
+    CGContextRetain(context);
 
-  // The device object will take ownership of the graphics context.
-  return new BitmapPlatformDevice(
+  BitmapPlatformDevice* rv = new BitmapPlatformDevice(
       new BitmapPlatformDeviceData(context), bitmap);
+
+  // The device object took ownership of the graphics context with its own
+  // CGContextRetain call.
+  CGContextRelease(context);
+
+  return rv;
 }
 
 BitmapPlatformDevice* BitmapPlatformDevice::CreateWithData(uint8_t* data,
@@ -223,7 +230,14 @@ BitmapPlatformDevice* BitmapPlatformDevice::CreateWithData(uint8_t* data,
   if (data)
     context = CGContextForData(data, width, height);
 
-  return Create(context, width, height, is_opaque);
+  BitmapPlatformDevice* rv = Create(context, width, height, is_opaque);
+
+  // The device object took ownership of the graphics context with its own
+  // CGContextRetain call.
+  if (context)
+    CGContextRelease(context);
+
+  return rv;
 }
 
 // The device will own the bitmap, which corresponds to also owning the pixel