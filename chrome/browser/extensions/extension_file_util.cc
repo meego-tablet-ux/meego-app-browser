meego-tablet-ux/meego-app-browser

commit 2e2e5ea418b31bb3ae5cd044542f90a4d37e9b4b

Loads local resources from current locale subtree if available, if not it falls back to extension subtree.
We look for ext_root/foo/bar.js under ext_root/_locales/fr/foo/bar.js if current locale is fr. If there is no fr specific resource we load ext_root/foo/bar.js instead.

Lots of small refactoring to replace FilePath with ExtensionResource.


BUG=12131
TEST=See unittest for sample tree.


Review URL: http://codereview.chromium.org/256022

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28333 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: ee2e70de42647fe99de822d1f484a2b40590c8cf

chrome/browser/extensions/extension_file_util.cc
@@ -137,7 +137,7 @@ bool ValidateExtension(Extension* extension, std::string* error) {
   // Validate icons exist.
   for (std::map<int, std::string>::const_iterator iter =
        extension->icons().begin(); iter != extension->icons().end(); ++iter) {
-    if (!file_util::PathExists(extension->GetResourcePath(iter->second))) {
+    if (extension->GetResource(iter->second).GetFilePath().empty()) {
       *error = StringPrintf("Could not load extension icon '%s'.",
                             iter->second.c_str());
       return false;
@@ -172,8 +172,9 @@ bool ValidateExtension(Extension* extension, std::string* error) {
     const UserScript& script = extension->content_scripts()[i];
 
     for (size_t j = 0; j < script.js_scripts().size(); j++) {
-      const FilePath& path = script.js_scripts()[j].path();
-      if (!file_util::PathExists(path)) {
+      const FilePath& path =
+        script.js_scripts()[j].resource().GetFilePath();
+      if (path.empty()) {
         *error = StringPrintf("Could not load '%s' for content script.",
                               WideToUTF8(path.ToWStringHack()).c_str());
         return false;
@@ -181,8 +182,9 @@ bool ValidateExtension(Extension* extension, std::string* error) {
     }
 
     for (size_t j = 0; j < script.css_scripts().size(); j++) {
-      const FilePath& path = script.css_scripts()[j].path();
-      if (!file_util::PathExists(path)) {
+      const FilePath& path =
+        script.css_scripts()[j].resource().GetFilePath();
+      if (path.empty()) {
         *error = StringPrintf("Could not load '%s' for content script.",
                               WideToUTF8(path.ToWStringHack()).c_str());
         return false;
@@ -219,7 +221,7 @@ bool ValidateExtension(Extension* extension, std::string* error) {
     const std::vector<std::string>& icon_paths = page_action->icon_paths();
     for (std::vector<std::string>::const_iterator iter = icon_paths.begin();
          iter != icon_paths.end(); ++iter) {
-      if (!file_util::PathExists(extension->GetResourcePath(*iter))) {
+      if (extension->GetResource(*iter).GetFilePath().empty()) {
         *error = StringPrintf("Could not load icon '%s' for page action.",
                               iter->c_str());
         return false;
@@ -233,7 +235,7 @@ bool ValidateExtension(Extension* extension, std::string* error) {
     const std::vector<std::string>& icon_paths = browser_action->icon_paths();
     for (std::vector<std::string>::const_iterator iter = icon_paths.begin();
          iter != icon_paths.end(); ++iter) {
-      if (!file_util::PathExists(extension->GetResourcePath(*iter))) {
+      if (extension->GetResource(*iter).GetFilePath().empty()) {
         *error = StringPrintf("Could not load icon '%s' for browser action.",
                               iter->c_str());
         return false;

chrome/browser/extensions/crx_installer.cc
@@ -124,8 +124,9 @@ void CrxInstaller::OnUnpackSuccess(const FilePath& temp_dir,
   }
 
   if (client_.get()) {
-    DecodeInstallIcon(extension_->GetIconPath(Extension::EXTENSION_ICON_LARGE),
-                      &install_icon_);
+    FilePath icon_path =
+        extension_->GetIconPath(Extension::EXTENSION_ICON_LARGE).GetFilePath();
+    DecodeInstallIcon(icon_path, &install_icon_);
   }
   ui_loop_->PostTask(FROM_HERE, NewRunnableMethod(this,
       &CrxInstaller::ConfirmInstall));

chrome/browser/extensions/execute_code_in_tab_function.cc
@@ -7,7 +7,7 @@
 #include "chrome/browser/browser.h"
 #include "chrome/browser/extensions/extension_tabs_module.h"
 #include "chrome/browser/extensions/extension_tabs_module_constants.h"
-#include "chrome/browser/net/file_reader.h"
+#include "chrome/browser/extensions/file_reader.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_error_utils.h"
@@ -80,15 +80,15 @@ bool ExecuteCodeInTabFunction::RunImpl() {
   if (script_info->HasKey(kFileKey)) {
     if (!script_info->GetString(kFileKey, &relative_path))
       return false;
-    file_path_ = GetExtension()->GetResourcePath(relative_path);
+    resource_ = GetExtension()->GetResource(relative_path);
   }
-  if (file_path_.empty()) {
+  if (resource_.extension_root().empty() || resource_.relative_path().empty()) {
     error_ = keys::kNoCodeOrFileToExecuteError;
     return false;
   }
 
   scoped_refptr<FileReader> file_reader(new FileReader(
-      file_path_, NewCallback(this, &ExecuteCodeInTabFunction::DidLoadFile)));
+      resource_, NewCallback(this, &ExecuteCodeInTabFunction::DidLoadFile)));
   file_reader->Start();
   AddRef();  // Keep us alive until DidLoadFile is called.
 
@@ -102,10 +102,10 @@ void ExecuteCodeInTabFunction::DidLoadFile(bool success,
   } else {
 #if defined(OS_POSIX)
     error_ = ExtensionErrorUtils::FormatErrorMessage(keys::kLoadFileError,
-        file_path_.value());
+        resource_.relative_path().value());
 #elif defined(OS_WIN)
     error_ = ExtensionErrorUtils::FormatErrorMessage(keys::kLoadFileError,
-        WideToUTF8(file_path_.value()));
+        WideToUTF8(resource_.relative_path().value()));
 #endif  // OS_WIN
     SendResponse(false);
   }

chrome/browser/extensions/execute_code_in_tab_function.h
@@ -10,6 +10,7 @@
 
 #include "base/file_path.h"
 #include "chrome/browser/extensions/extension_function.h"
+#include "chrome/common/extensions/extension_resource.h"
 #include "chrome/common/notification_service.h"
 #include "chrome/common/notification_registrar.h"
 
@@ -40,8 +41,9 @@ class ExecuteCodeInTabFunction : public AsyncExtensionFunction,
   // Id of tab which executes code.
   int execute_tab_id_;
 
-  // Contain path of file which is specified in JSON arguments.
-  FilePath file_path_;
+  // Contains extension resource built from path of file which is
+  // specified in JSON arguments.
+  ExtensionResource resource_;
 };
 
 #endif  // CHROME_BROWSER_EXTENSIONS_EXECUTE_CODE_IN_TAB_FUNCTION_H__

chrome/browser/extensions/extension_disabled_infobar_delegate.cc
@@ -7,11 +7,13 @@
 #include "app/l10n_util.h"
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/extensions/crx_installer.h"
+#include "chrome/browser/extensions/extension_file_util.h"
 #include "chrome/browser/extensions/extension_install_ui.h"
 #include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/tab_contents/infobar_delegate.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
 #include "chrome/browser/browser_list.h"
+#include "chrome/common/extensions/extension_resource.h"
 #include "chrome/common/notification_registrar.h"
 #include "chrome/common/notification_service.h"
 #include "grit/generated_resources.h"
@@ -28,7 +30,7 @@ class ExtensionDisabledDialogDelegate
     AddRef();  // balanced in ContinueInstall or AbortInstall.
 
     // Do this now because we can't touch extension on the file loop.
-    install_icon_path_ =
+    install_icon_resource_ =
         extension_->GetIconPath(Extension::EXTENSION_ICON_LARGE);
 
     ChromeThread::GetMessageLoop(ChromeThread::FILE)->PostTask(FROM_HERE,
@@ -48,7 +50,8 @@ class ExtensionDisabledDialogDelegate
  private:
   void Start() {
     // We start on the file thread so we can decode the install icon.
-    CrxInstaller::DecodeInstallIcon(install_icon_path_, &install_icon_);
+    FilePath install_icon_path = install_icon_resource_.GetFilePath();
+    CrxInstaller::DecodeInstallIcon(install_icon_path, &install_icon_);
     // Then we display the UI on the UI thread.
     ui_loop_->PostTask(FROM_HERE,
         NewRunnableMethod(this,
@@ -64,7 +67,7 @@ class ExtensionDisabledDialogDelegate
   Profile* profile_;
   ExtensionsService* service_;
   Extension* extension_;
-  FilePath install_icon_path_;
+  ExtensionResource install_icon_resource_;
   scoped_ptr<SkBitmap> install_icon_;
   MessageLoop* ui_loop_;
 };

chrome/browser/extensions/extension_l10n_util.cc
@@ -144,4 +144,16 @@ ExtensionMessageBundle* LoadMessageCatalogs(
                                         error);
 }
 
+FilePath GetL10nRelativePath(const FilePath& relative_resource_path) {
+  // Get locale relative path.
+  static std::string current_locale = l10n_util::GetApplicationLocale(L"");
+  std::replace(current_locale.begin(), current_locale.end(), '-', '_');
+
+  FilePath locale_relative_path;
+  return locale_relative_path
+      .AppendASCII(Extension::kLocaleFolder)
+      .AppendASCII(current_locale)
+      .Append(relative_resource_path);
+}
+
 }  // namespace extension_l10n_util

chrome/browser/extensions/extension_l10n_util.h
@@ -55,6 +55,9 @@ ExtensionMessageBundle* LoadMessageCatalogs(const FilePath& locale_path,
                                             const std::string& app_locale,
                                             std::string* error);
 
+// Returns relative l10n path to the resource.
+FilePath GetL10nRelativePath(const FilePath& relative_resource_path);
+
 }  // namespace extension_l10n_util
 
 #endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_L10N_UTIL_H_

chrome/browser/extensions/extension_l10n_util_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/extensions/extension_l10n_util.h"
 
+#include "app/l10n_util.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/path_service.h"
@@ -141,6 +142,36 @@ TEST(ExtensionL10nUtil, LoadMessageCatalogsDuplicateKeys) {
                                                &error));
   EXPECT_TRUE(NULL != message_bundle.get());
   EXPECT_TRUE(error.empty());
+}
+
+bool PathsAreEqual(const FilePath& path1, const FilePath& path2) {
+  FilePath::StringType path1_str = path1.value();
+  std::replace(path1_str.begin(), path1_str.end(), '\\', '/');
+
+  FilePath::StringType path2_str = path2.value();
+  std::replace(path2_str.begin(), path2_str.end(), '\\', '/');
+
+  if (path1_str == path2_str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+TEST(ExtensionL10nUtil, GetL10nRelativePath) {
+  static std::string current_locale = l10n_util::GetApplicationLocale(L"");
+  std::replace(current_locale.begin(), current_locale.end(), '-', '_');
+
+  FilePath locale_path;
+  locale_path = locale_path
+      .AppendASCII(Extension::kLocaleFolder)
+      .AppendASCII(current_locale)
+      .AppendASCII("foo")
+      .AppendASCII("bar.js");
+
+  FilePath result = extension_l10n_util::GetL10nRelativePath(
+      FilePath(FILE_PATH_LITERAL("foo/bar.js")));
+  EXPECT_TRUE(PathsAreEqual(locale_path, result));
 }
 
 }  // namespace

commit 16a2f7be440c6b27eb91d8f559c38c78d92ba028

Fixes bug introduced in r28333, where we were checking if path.empty() instead of !PathExists(path). In addition makes background page load check.
BUG=24041
TEST=Try loading extension with content script that doesn't exist on disk, but is listed in manifest.

Review URL: http://codereview.chromium.org/335037

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30217 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 71825b4ef296dd18e8926969c96c61c2cc7d1576

chrome/browser/extensions/extension_file_util.cc
@@ -149,7 +149,8 @@ bool ValidateExtension(Extension* extension, std::string* error) {
   // Validate icons exist.
   for (std::map<int, std::string>::const_iterator iter =
        extension->icons().begin(); iter != extension->icons().end(); ++iter) {
-    if (extension->GetResource(iter->second).GetFilePath().empty()) {
+    const FilePath path = extension->GetResource(iter->second).GetFilePath();
+    if (!file_util::PathExists(path)) {
       *error = StringPrintf("Could not load extension icon '%s'.",
                             iter->second.c_str());
       return false;
@@ -187,7 +188,7 @@ bool ValidateExtension(Extension* extension, std::string* error) {
       const UserScript::File& js_script = script.js_scripts()[j];
       const FilePath& path = ExtensionResource::GetFilePath(
           js_script.extension_root(), js_script.relative_path());
-      if (path.empty()) {
+      if (!file_util::PathExists(path)) {
         *error = StringPrintf("Could not load '%s' for content script.",
                               WideToUTF8(path.ToWStringHack()).c_str());
         return false;
@@ -198,7 +199,7 @@ bool ValidateExtension(Extension* extension, std::string* error) {
       const UserScript::File& css_script = script.css_scripts()[j];
       const FilePath& path = ExtensionResource::GetFilePath(
           css_script.extension_root(), css_script.relative_path());
-      if (path.empty()) {
+      if (!file_util::PathExists(path)) {
         *error = StringPrintf("Could not load '%s' for content script.",
                               WideToUTF8(path.ToWStringHack()).c_str());
         return false;
@@ -235,7 +236,7 @@ bool ValidateExtension(Extension* extension, std::string* error) {
       icon_paths.push_back(page_action->default_icon_path());
     for (std::vector<std::string>::iterator iter = icon_paths.begin();
          iter != icon_paths.end(); ++iter) {
-      if (extension->GetResource(*iter).GetFilePath().empty()) {
+      if (!file_util::PathExists(extension->GetResource(*iter).GetFilePath())) {
         *error = StringPrintf("Could not load icon '%s' for page action.",
                               iter->c_str());
         return false;
@@ -247,13 +248,23 @@ bool ValidateExtension(Extension* extension, std::string* error) {
   // Note: browser actions don't use the icon_paths().
   ExtensionAction* browser_action = extension->browser_action();
   if (browser_action) {
-    std::string default_icon_path = browser_action->default_icon_path();
-    if (!default_icon_path.empty()) {
-      if (extension->GetResource(default_icon_path).GetFilePath().empty()) {
+    std::string path = browser_action->default_icon_path();
+    if (!path.empty() &&
+        !file_util::PathExists(extension->GetResource(path).GetFilePath())) {
         *error = StringPrintf("Could not load icon '%s' for browser action.",
-                              default_icon_path.c_str());
+                              path.c_str());
         return false;
-      }
+    }
+  }
+
+  // Validate background page location.
+  if (!extension->background_url().is_empty()) {
+    const std::string page_path = extension->background_url().path();
+    const FilePath path = extension->GetResource(page_path).GetFilePath();
+    if (!file_util::PathExists(path)) {
+      *error = StringPrintf("Could not load background page '%s'.",
+                            WideToUTF8(path.ToWStringHack()).c_str());
+      return false;
     }
   }
 