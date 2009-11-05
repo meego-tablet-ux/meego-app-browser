meego-tablet-ux/meego-app-browser

commit 4b53263ef6b2298e3e32d773ab59bd43d8134047

Only send renderer resize messages for RWHV sizing caused by TCV's size allocate message.  This avoids a spurious size-allocate being sent to the renderers when creating a new tab.  The extra size-allocate is called by the build-in GtkFixed's size allocate method.
Another way to think of this is that we only size our RWH directly
and decouple it's sizing from the GTK+ widget's sizing.

BUG=26495

Review URL: http://codereview.chromium.org/363012

git-svn-id: http://src.chromium.org/svn/trunk/src@31031 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

Former-commit-id: ce5db4ce8bc0eb971a4139e54834d16f77c4721c

chrome/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -66,8 +66,6 @@ class RenderWidgetHostViewGtkWidget {
                                   GDK_LEAVE_NOTIFY_MASK);
     GTK_WIDGET_SET_FLAGS(widget, GTK_CAN_FOCUS);
 
-    g_signal_connect(widget, "size-allocate",
-                     G_CALLBACK(SizeAllocate), host_view);
     g_signal_connect(widget, "expose-event",
                      G_CALLBACK(ExposeEvent), host_view);
     g_signal_connect(widget, "key-press-event",
@@ -100,14 +98,6 @@ class RenderWidgetHostViewGtkWidget {
   }
 
  private:
-  static gboolean SizeAllocate(GtkWidget* widget, GtkAllocation* allocation,
-                               RenderWidgetHostViewGtk* host_view) {
-    host_view->requested_size_ = gfx::Size(allocation->width,
-                                           allocation->height);
-    host_view->GetRenderWidgetHost()->WasResized();
-    return FALSE;
-  }
-
   static gboolean ExposeEvent(GtkWidget* widget, GdkEventExpose* expose,
                               RenderWidgetHostViewGtk* host_view) {
     const gfx::Rect damage_rect(expose->area);
@@ -417,8 +407,11 @@ void RenderWidgetHostViewGtk::SetSize(const gfx::Size& size) {
     gtk_widget_set_size_request(view_.get(), width, height);
 #endif
 
-    requested_size_ = gfx::Size(width, height);
-    host_->WasResized();
+    if (requested_size_.width() != width ||
+        requested_size_.height() != height) {
+      requested_size_ = gfx::Size(width, height);
+      host_->WasResized();
+    }
   }
 }
 

chrome/browser/tab_contents/interstitial_page.cc
@@ -457,13 +457,13 @@ void InterstitialPage::DontProceed() {
 }
 
 void InterstitialPage::SetSize(const gfx::Size& size) {
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_LINUX)
   // When a tab is closed, we might be resized after our view was NULLed
   // (typically if there was an info-bar).
   if (render_view_host_->view())
     render_view_host_->view()->SetSize(size);
 #else
-  // TODO(port): do Mac or Linux need to SetSize?
+  // TODO(port): Does Mac need to SetSize?
   NOTIMPLEMENTED();
 #endif
 }

chrome/browser/tab_contents/tab_contents_view_gtk.cc
@@ -398,14 +398,19 @@ gboolean TabContentsViewGtk::OnSizeAllocate(GtkWidget* widget,
                                             TabContentsViewGtk* view) {
   int width = allocation->width;
   int height = allocation->height;
-  view->requested_size_.set_width(width);
-  view->requested_size_.set_height(height);
   // |delegate()| can be NULL here during browser teardown.
   if (view->tab_contents()->delegate())
     height += view->tab_contents()->delegate()->GetExtraRenderViewHeight();
   gfx::Size size(width, height);
   view->requested_size_ = size;
   gtk_container_foreach(GTK_CONTAINER(widget), SetSizeRequest, &size);
+
+  // We manually tell our RWHV to resize the renderer content.  This avoids
+  // spurious resizes from GTK+.
+  if (view->tab_contents()->render_widget_host_view())
+    view->tab_contents()->render_widget_host_view()->SetSize(size);
+  if (view->tab_contents()->interstitial_page())
+    view->tab_contents()->interstitial_page()->SetSize(size);
 
   return FALSE;
 }

commit 0339ee9bc77992a02ff9275fa60399a82c6756b1

Tell the RWH about the intial window size for drop down menus. This fixes a bug where all drop down menus were blank (we never told the renderer our size).
This was regressed in r31031 when I removed the size allocate
handler.

BUG=26759

Review URL: http://codereview.chromium.org/361034

git-svn-id: http://src.chromium.org/svn/trunk/src@31127 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

Former-commit-id: 672c4563054d847644752f4dfefb8c7a8eeb8502

chrome/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -358,9 +358,11 @@ void RenderWidgetHostViewGtk::InitAsPopup(
     parent_host_view->Blur();
   }
 
-  gtk_widget_set_size_request(view_.get(),
-                              std::min(pos.width(), kMaxWindowWidth),
+  requested_size_ = gfx::Size(std::min(pos.width(), kMaxWindowWidth),
                               std::min(pos.height(), kMaxWindowHeight));
+  host_->WasResized();
+  gtk_widget_set_size_request(view_.get(), requested_size_.width(),
+                              requested_size_.height());
 
   gtk_window_set_default_size(GTK_WINDOW(popup), -1, -1);
   // Don't allow the window to be resized. This also forces the window to