meego-tablet-ux/meego-app-browser

commit d8608662a60dc3c7c1d466e9877226735691bb09

Find bar fixes:
- prefer previous find from same tab contents over overall most recent find when re-opening find bar
- GTK + Windows: always update find bar view text if it doesn't match the search results.

TEST=see bug for repro steps
BUG=30006

Review URL: http://codereview.chromium.org/482015

git-svn-id: http://src.chromium.org/svn/trunk/src@34322 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

Former-commit-id: 22be3c1fbd0e9df52abf5c6af805732f88df9594

chrome/browser/views/find_bar_view.cc
@@ -172,16 +172,12 @@ void FindBarView::UpdateForResult(const FindNotificationDetails& result,
   bool have_valid_range =
       result.number_of_matches() != -1 && result.active_match_ordinal() != -1;
 
-  // If we don't have any results and something was passed in, then that means
-  // someone pressed F3 while the Find box was closed. In that case we need to
-  // repopulate the Find box with what was passed in.
-  string16 search_string = find_text_->text();
-  if (search_string.empty() && !find_text.empty()) {
+  if (find_text_->text() != find_text) {
     find_text_->SetText(find_text);
     find_text_->SelectAll();
   }
 
-  if (!search_string.empty() && have_valid_range) {
+  if (!find_text.empty() && have_valid_range) {
     match_count_text_->SetText(
         l10n_util::GetStringF(IDS_FIND_IN_PAGE_COUNT,
                               IntToWString(result.active_match_ordinal()),
@@ -192,7 +188,7 @@ void FindBarView::UpdateForResult(const FindNotificationDetails& result,
     match_count_text_->SetText(std::wstring());
   }
 
-  if (search_string.empty() || result.number_of_matches() > 0 ||
+  if (find_text.empty() || result.number_of_matches() > 0 ||
       !have_valid_range) {
     // If there was no text entered or there were results, the match_count label
     // should have a normal background color. We also reset the background if

chrome/browser/gtk/find_bar_gtk.cc
@@ -333,12 +333,12 @@ void FindBarGtk::MoveWindowIfNecessary(const gfx::Rect& selection_rect,
 }
 
 void FindBarGtk::SetFindText(const string16& find_text) {
-  std::string text_entry_utf8 = UTF16ToUTF8(find_text);
+  std::string find_text_utf8 = UTF16ToUTF8(find_text);
 
   // Ignore the "changed" signal handler because programatically setting the
   // text should not fire a "changed" event.
   ignore_changed_signal_ = true;
-  gtk_entry_set_text(GTK_ENTRY(text_entry_), text_entry_utf8.c_str());
+  gtk_entry_set_text(GTK_ENTRY(text_entry_), find_text_utf8.c_str());
   ignore_changed_signal_ = false;
 }
 
@@ -356,20 +356,17 @@ void FindBarGtk::UpdateUIForFindResult(const FindNotificationDetails& result,
   if (result.number_of_matches() > 0)
     focus_store_.Store(NULL);
 
-  std::string text_entry_utf8 = UTF16ToUTF8(find_text);
+  std::string find_text_utf8 = UTF16ToUTF8(find_text);
   bool have_valid_range =
       result.number_of_matches() != -1 && result.active_match_ordinal() != -1;
 
-  // If we don't have any results and something was passed in, then that means
-  // someone pressed F3 while the Find box was closed. In that case we need to
-  // repopulate the Find box with what was passed in.
-  std::string search_string(gtk_entry_get_text(GTK_ENTRY(text_entry_)));
-  if (search_string.empty() && !text_entry_utf8.empty()) {
+  std::string entry_text(gtk_entry_get_text(GTK_ENTRY(text_entry_)));
+  if (entry_text != find_text_utf8) {
     SetFindText(find_text);
     gtk_entry_select_region(GTK_ENTRY(text_entry_), 0, -1);
   }
 
-  if (!search_string.empty() && have_valid_range) {
+  if (!find_text.empty() && have_valid_range) {
     gtk_label_set_text(GTK_LABEL(match_count_label_),
         l10n_util::GetStringFUTF8(IDS_FIND_IN_PAGE_COUNT,
             IntToString16(result.active_match_ordinal()),

chrome/browser/tab_contents/tab_contents.cc
@@ -246,7 +246,6 @@ TabContents::TabContents(Profile* profile,
       find_ui_active_(false),
       find_op_aborted_(false),
       current_find_request_id_(find_request_id_counter_++),
-      find_text_(),
       last_search_case_sensitive_(false),
       last_search_prepopulate_text_(NULL),
       last_search_result_(),
@@ -1020,10 +1019,14 @@ void TabContents::StartFinding(string16 search_string,
   // If search_string is empty, it means FindNext was pressed with a keyboard
   // shortcut so unless we have something to search for we return early.
   if (search_string.empty() && find_text_.empty()) {
-    if (last_search_prepopulate_text_->empty())
+    // Try the last thing we searched for on this tab, then the last thing
+    // searched for on any tab.
+    if (!previous_find_text_.empty())
+      search_string = previous_find_text_;
+    else if (!last_search_prepopulate_text_->empty())
+      search_string = *last_search_prepopulate_text_;
+    else
       return;
-    // Try whatever we searched for last in any tab.
-    search_string = *last_search_prepopulate_text_;
   }
 
   // Keep track of the previous search.
@@ -1061,6 +1064,7 @@ void TabContents::StopFinding(bool clear_selection) {
   // by the user, but the UI has not been dismissed.
   if (!clear_selection)
     find_ui_active_ = false;
+  previous_find_text_ = find_text_;
   find_text_.clear();
   find_op_aborted_ = true;
   last_search_result_ = FindNotificationDetails();

chrome/browser/views/find_bar_host_browsertest.cc
@@ -765,3 +765,38 @@ IN_PROC_BROWSER_TEST_F(FindInPageControllerTest, RestartSearchFromF3) {
   EXPECT_EQ(1, FindInPageWchar(tab, L"", kFwd, kIgnoreCase, &ordinal));
   EXPECT_EQ(1, ordinal);
 }
+
+// When re-opening the find bar with F3, the find bar should be re-populated
+// with the last search from the same tab rather than the last overall search.
+// http://crbug.com/30006
+IN_PROC_BROWSER_TEST_F(FindInPageControllerTest, PreferPreviousSearch) {
+  HTTPTestServer* server = StartHTTPServer();
+
+  DropdownBarHost::disable_animations_during_testing_ = true;
+
+  // First we navigate to any page.
+  GURL url = server->TestServerPageW(kSimplePage);
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  // Find "Default".
+  int ordinal = 0;
+  TabContents* tab1 = browser()->GetSelectedTabContents();
+  EXPECT_EQ(1, FindInPageWchar(tab1, L"Default", kFwd, kIgnoreCase, &ordinal));
+
+  // Create a second tab.
+  browser()->AddTabWithURL(url, GURL(), PageTransition::TYPED, true, -1,
+      false, NULL);
+  browser()->SelectTabContentsAt(1, false);
+  TabContents* tab2 = browser()->GetSelectedTabContents();
+  EXPECT_NE(tab1, tab2);
+
+  // Find "given".
+  FindInPageWchar(tab2, L"given", kFwd, kIgnoreCase, &ordinal);
+
+  // Switch back to first tab.
+  browser()->SelectTabContentsAt(0, false);
+  browser()->GetFindBarController()->EndFindSession();
+  // Simulate F3.
+  ui_test_utils::FindInPage(tab1, string16(), kFwd, kIgnoreCase, &ordinal);
+  EXPECT_EQ(tab1->find_text(), WideToUTF16(L"Default"));
+}

commit 30e9fcd2b3a5c283ad254b7b681208b998d82054

A quick fix for Issue 34970. This issue is caused by r34322 that changes text composed by an IME. (It makes Windows reset the IME.) As a quick fix, this changes adds a new method TextField::IMEIsComposing() and call it before changing text in FindBarView::UpdateForResult().
BUG=34970
TEST=Type Ctrl+F, enable a Japanese IME, and type shift+a, return, and shift+a. Verify if the find box has two As, i.e. 'AA'. 
Review URL: http://codereview.chromium.org/1630014

git-svn-id: http://src.chromium.org/svn/trunk/src@44904 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

Former-commit-id: 95a89ca62d26a1c4f153abc366d1fa0fbadabbf0

chrome/browser/views/find_bar_view.cc
@@ -179,7 +179,10 @@ void FindBarView::UpdateForResult(const FindNotificationDetails& result,
   bool have_valid_range =
       result.number_of_matches() != -1 && result.active_match_ordinal() != -1;
 
-  if (find_text_->text() != find_text) {
+  // http://crbug.com/34970: some IMEs get confused if we change the text
+  // composed by them. To avoid this problem, we should check the IME status and
+  // update the text only when the IME is not composing text.
+  if (find_text_->text() != find_text && !find_text_->IsIMEComposing()) {
     find_text_->SetText(find_text);
     find_text_->SelectAll();
   }

views/controls/textfield/native_textfield_gtk.cc
@@ -212,6 +212,10 @@ gfx::NativeView NativeTextfieldGtk::GetTestingHandle() const {
   return native_view();
 }
 
+bool NativeTextfieldGtk::IsIMEComposing() const {
+  return false;
+}
+
 // static
 gboolean NativeTextfieldGtk::OnKeyPressEventHandler(
     GtkWidget* entry,

views/controls/textfield/native_textfield_gtk.h
@@ -44,6 +44,7 @@ class NativeTextfieldGtk : public NativeControlGtk,
   virtual void SetFocus();
   virtual View* GetView();
   virtual gfx::NativeView GetTestingHandle() const;
+  virtual bool IsIMEComposing() const;
 
   // Overridden from NativeControlGtk:
   virtual void CreateNativeControl();

views/controls/textfield/native_textfield_win.cc
@@ -244,6 +244,20 @@ gfx::NativeView NativeTextfieldWin::GetTestingHandle() const {
   return m_hWnd;
 }
 
+bool NativeTextfieldWin::IsIMEComposing() const {
+  // Retrieve the length of the composition string to check if an IME is
+  // composing text. (If this length is > 0 then an IME is being used to compose
+  // text.)
+  HIMC imm_context = ImmGetContext(m_hWnd);
+  if (!imm_context)
+    return false;
+
+  const int composition_size = ImmGetCompositionString(imm_context, GCS_COMPSTR,
+                                                       NULL, 0);
+  ImmReleaseContext(m_hWnd, imm_context);
+  return composition_size > 0;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // NativeTextfieldWin, menus::SimpleMenuModel::Delegate implementation:
 

views/controls/textfield/native_textfield_win.h
@@ -62,6 +62,7 @@ class NativeTextfieldWin
   virtual void SetFocus();
   virtual View* GetView();
   virtual gfx::NativeView GetTestingHandle() const;
+  virtual bool IsIMEComposing() const;
 
   // Overridden from menus::SimpleMenuModel::Delegate:
   virtual bool IsCommandIdChecked(int command_id) const;

views/controls/textfield/native_textfield_wrapper.h
@@ -1,6 +1,6 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved. Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef VIEWS_CONTROLS_TEXTFIELD_NATIVE_TEXTFIELD_WRAPPER_H_
 #define VIEWS_CONTROLS_TEXTFIELD_NATIVE_TEXTFIELD_WRAPPER_H_
@@ -81,6 +81,9 @@ class NativeTextfieldWrapper {
 
   // Returns a handle to the underlying native view for testing.
   virtual gfx::NativeView GetTestingHandle() const = 0;
+
+  // Returns whether or not an IME is composing text.
+  virtual bool IsIMEComposing() const = 0;
 
   // Creates an appropriate NativeTextfieldWrapper for the platform.
   static NativeTextfieldWrapper* CreateWrapper(Textfield* field);

views/controls/textfield/textfield.cc
@@ -202,6 +202,10 @@ void Textfield::SyncText() {
     text_ = native_wrapper_->GetText();
 }
 
+bool Textfield::IsIMEComposing() const {
+  return native_wrapper_ && native_wrapper_->IsIMEComposing();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Textfield, View overrides:
 
@@ -260,7 +264,7 @@ bool Textfield::GetAccessibleRole(AccessibilityTypes::Role* role) {
 
 bool Textfield::GetAccessibleState(AccessibilityTypes::State* state) {
   DCHECK(state);
-  
+
   *state = 0;
 
   if (read_only())

views/controls/textfield/textfield.h
@@ -206,6 +206,9 @@ class Textfield : public View {
   // been deleted during a window close.
   void SyncText();
 
+  // Returns whether or not an IME is composing text.
+  bool IsIMEComposing() const;
+
 #ifdef UNIT_TEST
   gfx::NativeView GetTestingHandle() const {
     return native_wrapper_ ? native_wrapper_->GetTestingHandle() : NULL;