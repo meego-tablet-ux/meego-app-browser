meego-tablet-ux/meego-app-browser

commit c0095f244ef3782feb3af6672e10c4bedd67728f

This adds a "killed tab" page to be displayed in the case where a tab is killed instead of crashing.
It also adds "about:kill" to simulate kills.

On ChromeOS killed tabs will be reloaded as soon as the user clicks on them unless they are the only tab, or the currently focused tab is killed.

BUG=http://crosbug.com/10043, http://crosbug.com/10711
TEST=Built linux and ChromeOS Chrome and killed a tab, and then did about:crash and made sure that the "Aw, Snap!" page was displayed. Did about:kill and made sure kill page was displayed.

Review URL: http://codereview.chromium.org/6053012

git-svn-id: http://src.chromium.org/svn/trunk/src@71360 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

Former-commit-id: 5bd7b0c668d14a123f9775d2f20ac67438c36244

chrome/browser/ui/browser.cc
@@ -2626,6 +2626,16 @@ void Browser::TabSelectedAt(TabContentsWrapper* old_contents,
                             bool user_gesture) {
   DCHECK(old_contents != new_contents);
 
+  // On some platforms we want to automatically reload tabs that are
+  // killed when the user selects them.
+  if (user_gesture && new_contents->tab_contents()->crashed_status() ==
+        base::TERMINATION_STATUS_PROCESS_WAS_KILLED) {
+    const CommandLine& parsed_command_line = *CommandLine::ForCurrentProcess();
+    if (parsed_command_line.HasSwitch(switches::kReloadKilledTabs))
+      Reload(CURRENT_TAB);
+    return;
+  }
+
   // If we have any update pending, do it now.
   if (!chrome_updater_factory_.empty() && old_contents)
     ProcessPendingUIUpdates();

chrome/app/generated_resources.grd
@@ -6746,6 +6746,16 @@ Keep your key file in a safe place. You will need it to create new versions of y
         Something went wrong while displaying this webpage. To continue, press Reload or go to another page.
       </message>
 
+      <!-- Killed Tab Strings -->
+      <!-- TODO(gspencer): These are temporary values until real
+           values are supplied by UI team. See http://crosbug.com/10711 -->
+      <message name="IDS_KILLED_TAB_TITLE" desc="The title of the killed tab page that is shown when a tab is killed by the OS. This is intended to be a humorous exclamation of dismay.">
+        He's Dead, Jim!
+      </message>
+      <message name="IDS_KILLED_TAB_MESSAGE" desc="The message displayed on the killed tab page.">
+        Something caused this webpage to be killed, either because the operating system ran out of memory, or for some other reason. To continue, press Reload or go to another page.
+      </message>
+
       <!-- Incognito -->
       <message name="IDS_OFF_THE_RECORD_TOOLTIP" desc="The tooltip for the incognito icon.">
         You have opened an incognito window. Pages that you open in this window won't appear in your history.

chrome/app/theme/theme_resources.grd
@@ -289,6 +289,7 @@
       <include name="IDR_RESTORE_BUTTON_MASK" file="restore_button_mask.png" type="BINDATA" />
       <include name="IDR_SAD_FAVICON" file="sadfavicon.png" type="BINDATA" />
       <include name="IDR_SAD_TAB" file="sadtab.png" type="BINDATA" />
+      <include name="IDR_KILLED_TAB" file="killtab.png" type="BINDATA" />
       <include name="IDR_SEARCH_ENGINE_DIALOG_TOP" file="search_engine_top.png" type="BINDATA" />
       <if expr="pp_ifdef('_google_chrome')">
         <include name="IDR_SEARCH_ENGINE_LOGO_ABCSOK" file="google_chrome/search_abcsok.png" type="BINDATA" />

chrome/browser/browser_about_handler.cc
@@ -289,7 +289,8 @@ std::string AboutAbout() {
     html.append(kAllAboutPaths[i]);
     html.append("</a>\n");
   }
-  const char *debug[] = { "crash", "hang", "shorthang", "gpucrash", "gpuhang" };
+  const char *debug[] = { "crash", "kill", "hang", "shorthang",
+                          "gpucrash", "gpuhang" };
   html.append("</ul><h2>For Debug</h2>");
   html.append("</ul><p>The following pages are for debugging purposes only. "
               "Because they crash or hang the renderer, they're not linked "

chrome/browser/browser_about_handler_unittest.cc
@@ -66,6 +66,12 @@ TEST(BrowserAboutHandlerTest, WillHandleBrowserAboutURL) {
         false
       },
       {
+        GURL(chrome::kAboutKillURL),
+        GURL(chrome::kAboutKillURL),
+        true,
+        false
+      },
+      {
         GURL(chrome::kAboutHangURL),
         GURL(chrome::kAboutHangURL),
         true,

chrome/browser/dom_ui/dom_ui_factory.cc
@@ -251,8 +251,9 @@ bool DOMUIFactory::IsURLAcceptableForDOMUI(Profile* profile, const GURL& url) {
       // It's possible to load about:blank in a DOM UI renderer.
       // See http://crbug.com/42547
       url.spec() == chrome::kAboutBlankURL ||
-      // about:crash, about:hang, and about:shorthang are allowed.
+      // about:crash, about:kill, about:hang, and about:shorthang are allowed.
       url.spec() == chrome::kAboutCrashURL ||
+      url.spec() == chrome::kAboutKillURL ||
       url.spec() == chrome::kAboutHangURL ||
       url.spec() == chrome::kAboutShorthangURL;
 }

chrome/browser/gtk/sad_tab_gtk.cc
@@ -19,8 +19,13 @@
 
 namespace {
 
-// Background color of the content (a grayish blue).
-const GdkColor kBackgroundColor = GDK_COLOR_RGB(35, 48, 64);
+// Background color of the content (a grayish blue) for a crashed tab.
+const GdkColor kCrashedBackgroundColor = GDK_COLOR_RGB(35, 48, 64);
+
+// Background color of the content (a grayish purple) for a killed
+// tab.  TODO(gspencer): update this for the "real" color when the UI
+// team provides one.  See http://crosbug.com/10711
+const GdkColor kKilledBackgroundColor = GDK_COLOR_RGB(57, 48, 88);
 
 // Construct a centered GtkLabel with a white foreground.
 // |format| is a printf-style format containing a %s;
@@ -44,13 +49,17 @@ GtkWidget* MakeWhiteMarkupLabel(const char* format, const std::string& str) {
 
 }  // namespace
 
-SadTabGtk::SadTabGtk(TabContents* tab_contents)
-  : tab_contents_(tab_contents) {
+SadTabGtk::SadTabGtk(TabContents* tab_contents, Kind kind)
+    : tab_contents_(tab_contents),
+      kind_(kind) {
   DCHECK(tab_contents_);
 
   // Use an event box to get the background painting correctly.
   event_box_.Own(gtk_event_box_new());
-  gtk_widget_modify_bg(event_box_.get(), GTK_STATE_NORMAL, &kBackgroundColor);
+  gtk_widget_modify_bg(event_box_.get(), GTK_STATE_NORMAL,
+                       kind == CRASHED ?
+                       &kCrashedBackgroundColor :
+                       &kKilledBackgroundColor);
   // Allow ourselves to be resized arbitrarily small.
   gtk_widget_set_size_request(event_box_.get(), 0, 0);
 
@@ -63,7 +72,9 @@ SadTabGtk::SadTabGtk(TabContents* tab_contents)
 
   // Add center-aligned image.
   GtkWidget* image = gtk_image_new_from_pixbuf(
-      ResourceBundle::GetSharedInstance().GetPixbufNamed(IDR_SAD_TAB));
+      ResourceBundle::GetSharedInstance().GetPixbufNamed(kind == CRASHED ?
+                                                         IDR_SAD_TAB :
+                                                         IDR_KILLED_TAB));
   gtk_misc_set_alignment(GTK_MISC(image), 0.5, 0.5);
   gtk_box_pack_start(GTK_BOX(vbox), image, FALSE, FALSE, 0);
 
@@ -75,7 +86,9 @@ SadTabGtk::SadTabGtk(TabContents* tab_contents)
   // Add center-aligned title.
   GtkWidget* title = MakeWhiteMarkupLabel(
       "<span size=\"larger\" style=\"normal\"><b>%s</b></span>",
-      l10n_util::GetStringUTF8(IDS_SAD_TAB_TITLE));
+      l10n_util::GetStringUTF8(kind == CRASHED ?
+                               IDS_SAD_TAB_TITLE :
+                               IDS_KILLED_TAB_TITLE));
   gtk_box_pack_start(GTK_BOX(vbox), title, FALSE, FALSE, 0);
 
   // Add spacer between title and message.
@@ -83,9 +96,11 @@ SadTabGtk::SadTabGtk(TabContents* tab_contents)
   gtk_box_pack_start(GTK_BOX(vbox), spacer, FALSE, FALSE, 0);
 
   // Add center-aligned message.
-  GtkWidget* message =
-      MakeWhiteMarkupLabel("<span style=\"normal\">%s</span>",
-                           l10n_util::GetStringUTF8(IDS_SAD_TAB_MESSAGE));
+  GtkWidget* message = MakeWhiteMarkupLabel(
+      "<span style=\"normal\">%s</span>",
+      l10n_util::GetStringUTF8(kind == CRASHED ?
+                               IDS_SAD_TAB_MESSAGE :
+                               IDS_KILLED_TAB_MESSAGE));
   gtk_label_set_line_wrap(GTK_LABEL(message), TRUE);
   gtk_box_pack_start(GTK_BOX(vbox), message, FALSE, FALSE, 0);
 
@@ -115,7 +130,10 @@ SadTabGtk::~SadTabGtk() {
 void SadTabGtk::OnLinkButtonClick(GtkWidget* sender) {
   if (tab_contents_ != NULL) {
     GURL help_url =
-        google_util::AppendGoogleLocaleParam(GURL(chrome::kCrashReasonURL));
+        google_util::AppendGoogleLocaleParam(GURL(
+            kind_ == CRASHED ?
+            chrome::kCrashReasonURL :
+            chrome::kKillReasonURL));
     tab_contents_->OpenURL(help_url, GURL(), CURRENT_TAB, PageTransition::LINK);
   }
 }

commit 2495dbdafb9605cf8c21fdbb972e377e3af39f2b

Update toolbar for killed tabs
Switching to a killed tab wouldn't update the toolbar.

This was a regression introduced by r71360. We were checking to see switches::kReloadKilledTabs was set and if so we were reloading the tab and skipping the update.

Unfortunately we were also skipping the update if switches::kReloadKilledTabs was not set. Fix was to only skip the update if switches::kReloadKilledTabs was set.

BUG=70306
TEST=Reproduced the bug. Applied my fix. Verified that the toolbar correctly updates for killed tabs.
Review URL: http://codereview.chromium.org/6312102

git-svn-id: http://src.chromium.org/svn/trunk/src@73534 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

Former-commit-id: bc75c6512d76d0bdfe8b577e9c324181b4abd367

chrome/browser/ui/browser.cc
@@ -2678,9 +2678,10 @@ void Browser::TabSelectedAt(TabContentsWrapper* old_contents,
   if (user_gesture && new_contents->tab_contents()->crashed_status() ==
         base::TERMINATION_STATUS_PROCESS_WAS_KILLED) {
     const CommandLine& parsed_command_line = *CommandLine::ForCurrentProcess();
-    if (parsed_command_line.HasSwitch(switches::kReloadKilledTabs))
+    if (parsed_command_line.HasSwitch(switches::kReloadKilledTabs)) {
       Reload(CURRENT_TAB);
-    return;
+      return;
+    }
   }
 
   // If we have any update pending, do it now.