meego-tablet-ux/meego-app-browser

commit d526d3d885e4744d919309e322cf5b67a3a75286

Session restore: enable bookmark and download manager restore
o Skip --cmd used by qml launcher and don't pass it to chromium
  command line handler
o Enable bookmark and download manager restore.

TODO:
No render process for new tab, download and bookmark manager

Reviewed by Yongsheng.

chrome/browser/qt/meego-app-browser/BrowserWindowTablet.qml
@@ -158,19 +158,41 @@ Item {
      onShowDownloads: {
        showdownloadmanager = is_show;
        if (is_show){
-         panelstring = qsTr("Downloads");
-         showbookmarkmanager = false;
+         if(downloadsLoader.source == "") {
+           var mappedPos = scene.mapToItem (outerContent, 0, toolbar.height + statusbar.height)
+           downloadsLoader.opacity = 1
+           downloadsLoader.source = "Downloads.qml"
+           downloadsHolder.initx = mappedPos.x
+           downloadsHolder.inity = mappedPos.y
+           downloadsLoader.item.showed = true
+         }
+         showdownloadmanager = true
+         panelstring  = downloadTitle
+         if (bookmarkManagerLoader.item)
+             showbookmarkmanager = false
        } else {
          downloadsLoader.source = "";
+         showdownloadmanager = false;
        }
      }
      onShowBookmarks: {
        showbookmarkmanager = is_show;
        if (is_show) {
-         panelstring = qsTr("Bookmarks");
-         showdownloadmanager = false;
+         if(bookmarkManagerLoader.source=="") {
+            var mappedPos = scene.mapToItem (outerContent, 0, toolbar.height + statusbar.height)
+            bookmarkManagerLoader.opacity = 1
+            bookmarkManagerLoader.source = "BookmarkList.qml"
+            bookmarkManagerLoader.item.portrait = !isLandscapeView()
+            bookmarkManagerHolder.initx = mappedPos.x
+            bookmarkManagerHolder.inity = mappedPos.y
+          }
+          showbookmarkmanager = true
+          panelstring  = bookmarkManagerTitle
+          if (downloadsLoader.item)
+             showdownloadmanager = false
        } else {
          bookmarkManagerLoader.source = "" 
+         showbookmarkmanager = false
        }
      } 
    }

chrome/browser/ui/meegotouch/bookmark_qt.h
@@ -328,6 +328,7 @@ public:
 
   void Init(Profile* profile, BookmarkOthersQt* others);
 
+  void ShowBookmarkManager();
 private:
   void ShowInstruction();
   // Helper function which generates GtkToolItems for |bookmark_toolbar_|.
@@ -346,8 +347,6 @@ private:
   // is equivalent to the number of children the bookmark bar node from the
   // bookmark bar model has.
   int GetBookmarkButtonCount();
-
-  void ShowBookmarkManager();
 
   // Set the appearance of the overflow button appropriately (either chromium
   // style or GTK style).

base/command_line.cc
@@ -194,8 +194,15 @@ CommandLine CommandLine::FromString(const std::wstring& command_line) {
 
 #if defined(OS_POSIX)
 void CommandLine::InitFromArgv(int argc, const char* const* argv) {
-  for (int i = 0; i < argc; ++i)
+  for (int i = 0; i < argc; ++i) {
+    // Since --cmd parameter is only for meego-qml-launcher
+    // Don't pass this parameter to chromium
+    if(strcmp(argv[i], "--cmd") == 0)
+    {
+      i++; continue;
+    }
     argv_.push_back(argv[i]);
+  }
   InitFromArgv(argv_);
 }
 

chrome/app/chrome_main.cc
@@ -250,6 +250,8 @@ void InitQmlLauncher(const std::string process_type, int& argc, char** argv)
   initAtoms ();
 
   g_main_window = new LauncherWindow(fullscreen, width, height, opengl, setSource);
+//    g_main_window = new LauncherWindow();
+//    g_main_window->init(fullscreen, width, height, opengl, setSource);
 
   //set appmode according to argv
   QDeclarativeContext *context = g_main_window->getDeclarativeView()->rootContext();

chrome/browser/ui/meegotouch/browser_window_qt.cc
@@ -126,6 +126,7 @@ class BrowserWindowQtImpl : public QObject
   }
   void ShowBookmarks(bool is_show)
   {
+     window_->bookmark_bar_->ShowBookmarkManager();
      emit showBookmarks(is_show);
   }
   void ShowDownloads(bool is_show)

commit 0cdc725c5d76ccb4cca8b1cd1bfcf71a276b1b4d

Fix BMC 21924 [REG] Bookmark Manager fails to change its layout according to orientation
This bug was introduced by commit d526d3d88 of Session restore to enable
bookmark manager restore.

The restore code added a new way to show bookmark manager, but it sets
portrait property to a fix value, which makes bookmark manager fails to
change its layout according to system's orientation on the fly.

This commit also removed out-of-date way of showing bookmark manager.

Reviewed by Gao Chun.


Former-commit-id: b1840b03ce17f24ed358560bb24caf3e2867df3a

chrome/browser/qt/meego-app-browser/BrowserWindowTablet.qml
@@ -184,7 +184,7 @@ Item {
             var mappedPos = scene.mapToItem (outerContent, 0, toolbar.height + statusbar.height)
             bookmarkManagerLoader.opacity = 1
             bookmarkManagerLoader.source = "BookmarkList.qml"
-            bookmarkManagerLoader.item.portrait = !isLandscapeView()
+            //bookmarkManagerLoader.item.portrait = !isLandscapeView()
             bookmarkManagerHolder.initx = mappedPos.x
             bookmarkManagerHolder.inity = mappedPos.y
           }

chrome/browser/ui/browser.cc
@@ -1892,10 +1892,6 @@ void Browser::ToggleBookmarkBar() {
 void Browser::OpenBookmarkManager() {
   UserMetrics::RecordAction(UserMetricsAction("ShowBookmarkManager"), profile_);
   ShowSingletonTab(GURL(chrome::kChromeUIBookmarksURL));
-  NotificationService::current()->Notify(
-    NotificationType::BOOKMARK_LIST_VISIBILITY_SHOW,
-    Source<Browser>(this),
-    NotificationService::NoDetails());
 }
 
 void Browser::ShowAppMenu() {

chrome/browser/ui/meegotouch/bookmark_qt.cc
@@ -589,8 +589,6 @@ BookmarkBarQt::BookmarkBarQt(BrowserWindowQt* window,
 //  SetProfile(profile);
   registrar_.Add(this, NotificationType::BROWSER_THEME_CHANGED,
                  NotificationService::AllSources());
-  registrar_.Add(this, NotificationType::BOOKMARK_LIST_VISIBILITY_SHOW,
-                 NotificationService::AllSources());
 }
 
 BookmarkBarQt::~BookmarkBarQt() {
@@ -820,8 +818,6 @@ void BookmarkBarQt::ShowBookmarkManager() {
     CreateAllBookmarkTreeItems();
     BookmarkList::started = true;
   }
-  tree_filter_->OpenBookmarkManager();
-  //grid_filter_->OpenBookmarkManager();
 }
 
 void BookmarkBarQt::Observe(NotificationType type,
@@ -838,8 +834,6 @@ void BookmarkBarQt::Observe(NotificationType type,
                   << "don't have a BookmarkModel. Taking no action.";
     }
 
-  } else if (type == NotificationType::BOOKMARK_LIST_VISIBILITY_SHOW) {
-    ShowBookmarkManager();
   } else {
     NOTREACHED();
   }

chrome/browser/ui/meegotouch/bookmark_qt.h
@@ -579,7 +579,6 @@ public:
   BookmarkQtFilterProxyModel(BookmarkQtImpl *impl, QObject *parent = 0);
   void Show() { emit show(); }
   void Hide() { emit hide(); }
-  void OpenBookmarkManager() { emit openBookmarkManager(); }
   void CloseBookmarkManager(){ emit closeBookmarkManager(); }
 //  void MoveToAnother() { emit moveToAnother(); }
 
@@ -593,7 +592,6 @@ public:
 Q_SIGNALS:
   void show();
   void hide();
-  void openBookmarkManager();
   void closeBookmarkManager();
 //  void moveToAnother();
 //